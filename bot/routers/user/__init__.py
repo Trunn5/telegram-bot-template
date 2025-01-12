17eda3def8ff885267d045042bbfbcb12556817d
Dmitrii Prosvirkin <prosvirkin.dmitry@gmail.com>
2025-01-13 00:10:44 +0500

    fix filtering start users handler

--- bot/routers/user/__init__.py
+++ bot/routers/user/__init__.py
@@ -1,8 +1,13 @@
 from aiogram import Router
+from aiogram import filters
 
 router = Router()
 
 from bot.routers.user.start import handle_start
 
 
-router.message.register(handle_start, ...)
+router.message.register(handle_start, filters.Command("start"))
+
+
+
+
